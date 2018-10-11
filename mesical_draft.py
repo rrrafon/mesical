49730d029bc11bb3872d45e4c402790a3402836d
runan <rrrafon@gmail.com>
2018-10-12 01:16:39 +1300

    first version

--- mesical_draft.py
+++ mesical_draft.py
@@ -1,76 +1,64 @@
-import glob, re
+import glob
 import subprocess
 
-TMPDIR = r'/home/runan/Documents/mesical/temp/'
 DIR = r'/home/runan/Documents/mesical/clips/'
+MUSIC = ('C2 C2 G2 G1 G2 G2 A4 F1 G1')
+WHOLENOTE = 500 #based on 120 bpm
+#TMPDIR = r'/home/runan/Documents/mesical/temp/'
 #MUSIC = ('D1 CS1 D1 D1 CS1 A1 E1 FS1 D1 D1 CS1 B1 CS1 FS1 A1 B1 G1 FS1 E1 G1 FS1 E1 D1 CS1 B1 A1 G1 FS1 E1 G1 FS1 E1 G1 FS1 E1 G1 FS1 E1 D1 E1 FS1 G1 A1 E1 A1 G1 FS1 B1 G1 A1 G1 D1 B1 B1 CS1 D1 CS1 B1 A1 G1 FS1 E1 B1 A1 B1 A1 G1 FS1 FS1 E1 D1 FS1 B1 A1 B1 CS1 D1 D1 CS1 B1 D1 D1 D1' )
 #MUSIC = ('C C G G A A G G F F E E D D C C G G F F E E D D G G F F E E D D C C G G A A G G F F E E D D C C')
-MUSIC = ('C2 C2 G2 G1 G2 G2 A4 F1 G1')
 #MUSIC = ('A1 G1 C1')
-TAIL = .001#in seconds
 
 
 def musicNoteDir(filePath = DIR, ext = '.mp3'):
+    '''
+    This script returns an array of FILES based on given file format
+    from the specified folder
 
+    :param filePath: file path to the musical notes
+    :param ext: file format of music or video file
+    :return: returns an array of files based on file format
+    '''
     try:
         # Load all files in Published Dir
         filesInDir = glob.glob("%s*%s" % (filePath, ext))
     except TypeError:
+        # catches an invalid file path
         print('please input file path')
         return
     mp3filesInDir = [i for i in filesInDir]
+
     return mp3filesInDir
 
-def test_All():
-    cmdFile = ''
-    musicFiles = musicNoteDir()
-    for i in musicFiles:
-        cmdFile += '%s|' % i
-    #command line for joining audio/video as one file
-    cmd = 'ffmpeg -i "concat:%s" -c copy output.mp3'%cmdFile
-    #calling window OS cmd line to run function
-    #subprocess.call(cmd, shell=True)
-    print(cmdFile)
 
-def mixAll(dir = DIR, ext = '.mp3' ):
-    #test on music playing
-    cont = ''
-
-    for i in MUSIC.split(' '):
-        cont+=('%s%s%s|'%(dir, i, ext))
-        #cont += ('-i %s%s%s '% (dir, i, ext))
-
-    cmd = 'ffmpeg -i "concat:%s" -c copy -y output.mp3' % cont
-    #cmd = 'ffmpeg %s -filter_complex acrossfade=d=%s:c1=qua:c2=qua -y output.mp3' % (cont, TAIL)
-
-    #print(cmd)
-    subprocess.call(cmd, shell=True)
-
-    #cmd2 = 'ffmpeg -i output.mp3 -filter:a "atempo=2" -vn output2.mp3'
-    #subprocess.call(cmd2, shell=True)
-
-
-def mixAllfade(dir = DIR,   ext = '.mp3', tempDir = TMPDIR, tempName = 'TMP101' ):
-    #test on music playing
+def mixAllfade(dir = DIR,   ext = '.mp3', fileName = 'mixedOutput'):
+    '''
+    This script mixes the audio/video based on the given musical chord list
+    :param dir: file directory of musical chords
+    :param ext: file format of the musical chords, either a video or audio
+    :return: null
+    '''
+    #   this var splits the list of MUSIC into individual chords
     notes = MUSIC.split(' ')
-    #input1 = ''
-    #input2 o= ''
-    print(len(notes))
-    for i in range(len(notes)):
-        input1 = ('%s%s%s ' % (dir, notes[i], ext))
-        try:
-            input2 = ('%s%s%s ' % (dir, notes[i+1], ext))
-        except IndexError:
-            pass
-        tempOut = '%s%s%s' % (tempDir, tempName, ext)
-        if i == 0:
-            cmd = 'ffmpeg -i %s -i %s -filter_complex acrossfade=d=%s -y %s' % (input1, input2, TAIL, tempOut )
-        elif i == len(notes)-1:
-            cmd = 'ffmpeg -i %s -i %s -filter_complex acrossfade=d=%s -y mixed_audio%s' % (tempOut, input1, TAIL, ext)
-        else:
-            cmd = 'ffmpeg -i %s -i %s -filter_complex acrossfade=d=%s -y %s' % (tempOut, input1, TAIL, tempOut)
+    #   count of notes in Music var
+    lenNotes = len(notes)
+    #   this var will store a list of audio/video file of music
+    chords = ''
+    #   this car will store a list of delay filter_complex for ffmpeg
+    delays = ''
+    mix = ''
+    #   the start time of music in milliseconds
+    musicTime = 0
 
-        print(cmd)
-        subprocess.call(cmd, shell=True)
+    for i in range(lenNotes):
+        chords += '-i %s%s '%(notes[i][0], ext)
+        delays += '[%s]adelay=%s|delay%s[chord%s];' % (str(i+1), musicTime, str(i+1), str(i+1))
+        mix += '[chord%s]' % str(i+1)
+        musicTime += int(WHOLENOTE/int(notes[i][1]))
+    cmd =  'ffmpeg %s -filter_complex %s [0]%s amix=%s %s%s' % (chords, delays, mix, lenNotes+1, fileName, ext)
+    print(cmd)
+
+    #   calling window OS cmd line to run function
+    #subprocess.call(cmd, shell=True)
 
 
